2a86d2c5d7acb4c12c0e27b3cfa0b871df369b6f
Leire <leisancabalei@gmail.com>
2024-01-27 18:18:11 +0100

    primer drag and drop + primer scroll correcto

--- datos-reto/web/main/mapa.js
+++ datos-reto/web/main/mapa.js
@@ -1,5 +1,50 @@
-window.addEventListener('load', event => {
-    if(localStorage.getItem('token') == null) {
-        window.location = "../index.html"
-    }
-})
+// window.addEventListener('load', event => {
+//     if(localStorage.getItem('token') == null) {
+//         window.location = "../index.html"
+//     }
+// })
+
+$(document).ready(function () {
+
+    /*DRAG AND DROP*/
+    $('.icono_widget').attr('draggable', true);
+
+    $(".icono_widget").on("dragstart", function (event) {
+        if (this.children.length) {
+            event.originalEvent.dataTransfer.setData('text', this.id);
+        } else {
+            event.preventDefault();
+        }
+        //console.log(this.id);
+    });
+
+    $("#widgets").on("dragover", function (event) {
+        event.preventDefault();
+        console.log(this.id);
+    })
+
+    $("#widgets").on("drop", function (event) {
+        event.preventDefault();
+        // console.log(this.id);
+        const id_widget = event.originalEvent.dataTransfer.getData('text')
+        switch (id_widget) {
+            case "sol":
+                // $('#sol').appendTo(this)
+                break;
+
+            case "lluvias":
+                // $('#lluvias').appendTo(this)
+                break;
+
+            case "viento":
+                // $('#viento').appendTo(this)
+                $('#viento').hide()
+                $('#widget_viento').show()
+                break;
+
+            default:
+                break;
+        }
+    });
+
+});
